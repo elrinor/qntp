cf881e30bbd146b1e9e63bb6bb228889c082905b
Elric <ru.elric@gmail.com>
2011-09-23 05:59:31 +0400

    + Can now be used by including all sources.

---
+++ include/qntp/src.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file
+ * 
+ * Include this file into your project if you don't want to build 
+ * QNtp separately.
+ */
+#define QNTP_EXPORT
+
+#include "../../src/qntp/Client.cpp"
+#include "../../src/Reply.cpp"
